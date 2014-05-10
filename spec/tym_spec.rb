cacaaec9c94353ef89bc2d86368e2d4f2f31c06e
kouba_c <masubun00@gmail.com>
2014-05-10 23:07:11 +0900

    refacter spec

--- spec/tym_spec.rb
+++ spec/tym_spec.rb
@@ -5,8 +5,22 @@ describe Tym do
     expect(Tym::VERSION).not_to be nil
   end
 
-  it 'can execute with valid option' do
-    Tym::Writer.execute("./spec/img/before.png", "./spec/txt/one_line_center.tym")
-    FileUtils::compare_file("./spec/img/before_tym.png", "./spec/img/after.png").should == true
+  describe Tym::Writer do
+    let(:img_path) { "./spec/img/" }
+    let(:txt_path) { "./spec/txt/" }
+
+    it 'can execute with valid option' do
+      input_img = img_path + "before.png"
+      input_txt = txt_path + "one_line_center.tym"
+      output_img = img_path + "before_tym.png"
+      validate_img = img_path + "after.png"
+
+      Tym::Writer.execute(input_img, input_txt) 
+      FileUtils::compare_file(output_img, validate_img).should == true
+    end
+
+    after do
+      FileUtils::rm(Dir.glob(img_path + "*_tym.png"))
+    end
   end
 end
