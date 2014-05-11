c59af8dc96a5c2bfe8b8094a8f528ec054fed841
kouba_c <masubun00@gmail.com>
2014-05-11 09:31:27 +0900

    Delete debug code

--- lib/tym/cli.rb
+++ lib/tym/cli.rb
@@ -4,7 +4,6 @@ module Tym
   class Cli
     def self.run(argv)
       params = argv.getopts('', 'overwrite')
-      p params
       raise if argv[0].nil?
       raise if argv[1].nil?
       Tym.execute(argv[0], argv[1], params['overwrite'])
